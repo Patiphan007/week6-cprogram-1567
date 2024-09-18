eff1fd54cbe09dbf9e09c1d250953ecac70b7dda
Patience <mtn.oppo59913@gmail.com>
2024-09-18 22:19:05 +0700

    first commit

---
+++ app701.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    int score = 81;
+    if(<score>= 80 && score<=100)
+        printf("Gradde A %d/n", score);
+    else if(score >= 70 && score <= 79)
+        printf("Gradde B %d/n", score);
+    else if (score >= 60 && score <= 69)
+        printf("Gradde c %d/n", score);
+    else if (score >= 50 && score <= 59)
+        printf("Gradde D %d/n", score);
+    else if (score >= 0 && score <= 49)
+        printf("Gradde F %d/n", score);
+    else
+        printf("ERROR/n");
+    printf("Finish");
+}
